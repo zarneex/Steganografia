4add1be0a03d17a0fefe6d20d29565cbedf21df7
zarneex <05vlasov@internet.ru>
2024-09-21 13:56:50 +0800

    Подключение библиотеки OpenCV, для отображения и загрузки изображения.  А так же пример кода

--- steganografia/steganografia.cpp
+++ steganografia/steganografia.cpp
@@ -1,6 +1,17 @@
-﻿
+﻿#include <opencv2/opencv.hpp>
 #include <iostream>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+using namespace cv;
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    Mat image = imread("C:\\practika_leto\\steganografia\\image.jpg");
+    if (image.empty()) {
+        cout << "Ошибка: не удалось загрузить изображение!" << endl;
+        return -1;
+    }
+    imshow("Отображение изображения", image);
+    waitKey(0);
+    return 0;
+}
